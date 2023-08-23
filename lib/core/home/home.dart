695bda401d39b9e2432cd5ce8baed7fc3f412285
Srujan <srujanmhase5@gmail.com>
2023-08-23 20:59:59 +0530

    Home UI

---
+++ lib/core/home/home.dart
@@ -0,0 +1,58 @@
+import 'package:employee_management/constants/colors.dart';
+import 'package:employee_management/constants/fontstyles.dart';
+import 'package:employee_management/core/home/employee_list.dart';
+import 'package:employee_management/cubit/app_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<AppCubit>().initialiseList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: scaffoldBg,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: themeBlue,
+        title: Text(
+          'Employee List',
+          style: titleOne,
+        ),
+      ),
+      body: context.watch<AppCubit>().state.employees.isEmpty
+          ? Center(
+              child: Image.asset('assets/empty.png'),
+            )
+          : const EmployeeList(),
+      floatingActionButton: SizedBox(
+        height: 50,
+        width: 50,
+        child: FloatingActionButton(
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          onPressed: () {},
+          child: const Center(
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
