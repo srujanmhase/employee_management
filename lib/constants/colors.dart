7e682a14e1f06780cbe91976d246a81703e4d8a2
Srujan <srujanmhase5@gmail.com>
2023-08-23 20:59:52 +0530

    Add: constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const themeBlue = Color(0xff1DA1F2);
+const themeLightBlue = Color(0xffEDF8FF);
+
+const themeDark = Color(0xff323238);
+const themeGrey = Color(0xff949C9E);
+
+const scaffoldBg = Color(0xffF2F2F2);
---
+++ lib/constants/fontstyles.dart
@@ -0,0 +1,63 @@
+import 'package:employee_management/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final titleOne = GoogleFonts.roboto(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  color: Colors.white,
+);
+
+final hintStyle = GoogleFonts.roboto(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  color: themeGrey,
+);
+
+final formText = GoogleFonts.roboto(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  color: themeDark,
+); //Modify for Calendar Header
+
+final bottomSheetText = GoogleFonts.roboto(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  color: themeDark,
+);
+
+final formSubmitPrimary = GoogleFonts.roboto(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: Colors.white,
+);
+
+final formSubmitSecondary = GoogleFonts.roboto(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: themeBlue,
+);
+
+final homePageHeader = GoogleFonts.roboto(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  color: themeBlue,
+);
+
+final employeeCardName = GoogleFonts.roboto(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  color: themeDark,
+);
+
+final employeeCardDesignation = GoogleFonts.roboto(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  color: themeGrey,
+);
+
+final employeeCardDates = GoogleFonts.roboto(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  color: themeGrey,
+);
