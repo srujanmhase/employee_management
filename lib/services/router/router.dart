c4e21e849dd66ebd29e061b59dc3073fee11ead8
Srujan <srujanmhase5@gmail.com>
2023-08-24 03:41:59 +0530

    ref to edit page in router

--- lib/services/router/router.dart
+++ lib/services/router/router.dart
@@ -1,19 +1,29 @@
+import 'package:employee_management/core/edit/cubit/edit_cubit.dart';
+import 'package:employee_management/core/edit/view/edit_view.dart';
 import 'package:employee_management/core/home/home.dart';
+import 'package:employee_management/models/employee.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 final routes = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(
-      path: '/',
-      name: '/',
+      path: HomePage.path,
+      name: HomePage.path,
       builder: (BuildContext context, state) => const HomePage(),
     ),
-    // GoRoute(
-    //   path: '/details',
-    //   name: '/details',
-    //   //builder: add/edit screen based on extra param
-    // )
+    GoRoute(
+      path: EditPage.path,
+      name: EditPage.path,
+      builder: (context, state) {
+        final employee = state.extra as Employee?;
+        return BlocProvider(
+          create: (context) => EditCubit(),
+          child: EditPage(employee: employee),
+        );
+      },
+    )
   ],
 );
